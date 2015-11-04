a1ffaf8292dbeef2ef0687c5ee36b06860e6e4bb
Oluwatoni <funmitoni2@yahoo.co.uk>
2015-11-04 08:57:15 -0500

    Modals now have links that work

---
+++ js/openModal.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	if(window.location.href.indexOf('#portfolioModal1') != -1) {
+    $('#portfolioModal1').modal('show');
+}
+$(document).ready(function() {
+	if(window.location.href.indexOf('#portfolioModal2') != -1) {
+    $('#portfolioModal2').modal('show');
+}
+$(document).ready(function() {
+	if(window.location.href.indexOf('#portfolioModal3') != -1) {
+    $('#portfolioModal3').modal('show');
+}
+$(document).ready(function() {
+	if(window.location.href.indexOf('#portfolioModal4') != -1) {
+    $('#portfolioModal4').modal('show');
+}
+$(document).ready(function() {
+	if(window.location.href.indexOf('#portfolioModal5') != -1) {
+    $('#portfolioModal5').modal('show');
+}
+$(document).ready(function() {
+	if(window.location.href.indexOf('#portfolioModal6') != -1) {
+    $('#portfolioModal6').modal('show');
+}
